8be2f9d4bf2975177e50a01d3ba4069b4c56b706
Alan Egerton <eggyal@gmail.com>
2021-11-02 14:53:24 +0000

    Initial commit

---
+++ update.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -eu
+
+git config --global user.email "eggyal+update.bot@gmail.com"
+git config --global user.name "Update Bot"
+
+sudo apt-get update -qq \
+    && cp "$MOLD_BUILD_DEPS" /tmp \
+    && sed "s/=.*$//g" "/tmp/$MOLD_BUILD_DEPS" \
+        | xargs apt-get install -qqsV \
+        | awk '$1 == "Conf" { gsub(/:.*/, "", $2); gsub(/^\(/, "", $3); print $2 "=" $3 }' \
+        | sort -sut= -k1,1 - "/tmp/$MOLD_BUILD_DEPS" \
+    > "$MOLD_BUILD_DEPS"
+
+git add -A
+git diff-index --quiet HEAD \
+    || git commit -qm 'Update mold build dependencies'
+
+curl -fsSL --proto '=https' --tlsv1.2 "https://api.github.com/repos/$MOLD_GITHUB_REPO/releases/latest" \
+    | jq -r .tarball_url \
+> "$MOLD_TARBALL_URL"
+
+git add -A
+git diff-index --quiet HEAD \
+    || git commit -qm 'Update mold'
+
+# git push & open PR
+exit 0
