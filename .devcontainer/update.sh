9208520e5b27057b81a084c39101265ebb0071e6
Alan Egerton <eggyal@gmail.com>
2021-12-03 11:39:32 +0000

    Add tooling for (rustc) perf analysis

--- .devcontainer/update.sh
+++ .devcontainer/update.sh
@@ -6,6 +6,7 @@ BUILD_CONTEXT="build-context"
 MOLD_GITHUB_REPO="rui314/mold"
 MOLD_TARBALL_URL="mold.url"
 MOLD_BUILD_DEPS="mold.build-packages"
+PACKAGES="packages"
 
 cd -- "$( dirname -- "${BASH_SOURCE[0]}" )"
 
@@ -27,5 +28,14 @@ update-scripts/latest-release-tarball.sh "$MOLD_GITHUB_REPO" \
 git add -A
 git diff-index --quiet HEAD \
     || git commit -qm 'Update mold'
+
+(
+    unlink "$BUILD_CONTEXT/$PACKAGES"
+    update-scripts/latest-packages.sh > "$BUILD_CONTEXT/$PACKAGES"
+) < "$BUILD_CONTEXT/$PACKAGES"
+
+git add -A
+git diff-index --quiet HEAD \
+    || git commit -qm 'Update packages'
 
 exit 0
