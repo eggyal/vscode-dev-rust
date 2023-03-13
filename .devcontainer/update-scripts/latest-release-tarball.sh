4b850310c75e9ecf428451d057c70622af7e0a4a
Alan Egerton <eggyal@gmail.com>
2023-03-13 10:37:06 +0000

    Use packaged mold binary

--- .devcontainer/update-scripts/latest-release-tarball.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-curl -fsSL --proto '=https' --tlsv1.2 "https://api.github.com/repos/$1/releases/latest" \
-    | jq -r .tarball_url
--- .devcontainer/update.sh
+++ .devcontainer/update.sh
@@ -3,31 +3,12 @@
 set -eu
 
 BUILD_CONTEXT="build-context"
-MOLD_GITHUB_REPO="rui314/mold"
-MOLD_TARBALL_URL="mold.url"
-MOLD_BUILD_DEPS="mold.build-packages"
 PACKAGES="packages"
 
 cd -- "$( dirname -- "${BASH_SOURCE[0]}" )"
 
 git config --global user.email "eggyal+update.bot@gmail.com"
 git config --global user.name "Update Bot"
-
-(
-    unlink "$BUILD_CONTEXT/$MOLD_BUILD_DEPS"
-    update-scripts/latest-packages.sh > "$BUILD_CONTEXT/$MOLD_BUILD_DEPS"
-) < "$BUILD_CONTEXT/$MOLD_BUILD_DEPS"
-
-git add -A
-git diff-index --quiet HEAD \
-    || git commit -qm 'Update mold build dependencies'
-
-update-scripts/latest-release-tarball.sh "$MOLD_GITHUB_REPO" \
-> "$BUILD_CONTEXT/$MOLD_TARBALL_URL"
-
-git add -A
-git diff-index --quiet HEAD \
-    || git commit -qm 'Update mold'
 
 (
     unlink "$BUILD_CONTEXT/$PACKAGES"
